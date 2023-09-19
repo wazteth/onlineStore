b08ea32d61063593d1fa8b999655f0dc749e8b85
wazteth <zawhtetmg111@gmail.com>
2023-09-19 22:18:09 +0630

    refactor index and about pages

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title', '$viewData["title"]')
+@section('title', $viewData["title"])
 @section('content')
 <div class="row">
     <div class="col-md-6 col-lg-4 mb-2">
