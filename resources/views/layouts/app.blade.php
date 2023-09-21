29827c86c17a97355b2be0874430c0a3d6416293
wazteth <zawhtetmg111@gmail.com>
2023-09-21 17:11:25 +0630

    List Products with Dummy array Data

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,31 +1,35 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        crossorigin="anonymous">
 
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
     <title>@yield('title', 'Online Store') </title>
-  </head>
-  <body>
+</head>
+
+<body>
     <!-- header -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-secondary py-4">
         <div class="container">
             <a href="{{ route('home.index')}}" class="navbar-brand">Online Store</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav ms-auto">
-                <a href="{{ route('home.index')}}" class="nav-link active">Home</a>
-                <a href="{{ route('home.about')}}" class="nav-link active">About</a>
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ms-auto">
+                    <a href="{{ route('home.index')}}" class="nav-link active">Home</a>
+                    <a href="{{ route('product.index')}}" class="nav-link active">Products</a>
+                    <a href="{{ route('home.about')}}" class="nav-link active">About</a>
+                </div>
             </div>
-        </div>
         </div>
     </nav>
 
@@ -34,7 +38,7 @@
             <h2>@yield('subtitle', 'A Laravel Online Store')</h2>
         </div>
     </header>
-<!-- header -->
+    <!-- header -->
 
     <div class="container my-4">
         @yield('content')
@@ -44,7 +48,8 @@
     <div class="copyright py-4 text-center text-white">
         <div class="container">
             <small>
-                Copyright - <a href="https://twitter.com/danielgarax" target="_blank" class="text-reset fw-bold text-decoration-none">
+                Copyright - <a href="https://twitter.com/danielgarax" target="_blank"
+                    class="text-reset fw-bold text-decoration-none">
                     Daniel Correa
                 </a> - <b>Paola Vallejo</b>
             </small>
@@ -54,12 +59,14 @@
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+    </script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
-  </body>
+</body>
+
 </html>
