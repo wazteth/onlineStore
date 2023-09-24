979a90a423573d27cdb23856fea2521e62d9a9e3
wazteth <zawhtetmg111@gmail.com>
2023-09-24 22:27:30 +0630

    create product with images

--- app/Http/Controllers/Admin/AdminProductController.php
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Product;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AdminProductController extends Controller
 {
@@ -32,6 +33,15 @@ class AdminProductController extends Controller
         $newProduct -> setImage("game.png");
         $newProduct -> save();
         
+        if($request->hasFile('image')){
+            $imageName = $newProduct->getId().".".$request->file('image')->extension();
+            Storage::disk('public')->put(
+                $imageName,
+                file_get_contents($request->file('image')->getRealPath())
+            );
+            $newProduct->setImage($imageName);
+            $newProduct->save();
+        }
         return back();
     }
     
